88db2fa4dd66c21fda84ccdc05a7ad09694f4795
Debasish Bhola <devop69@gmail.com>
2023-02-21 23:50:01 +0530

    Initial Commit
    Wrote the basics.
    I'll add the Java logic by tomorrow.

---
+++ app/src/main/java/com/example/appnumbershape/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.appnumbershape;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void testNumber(View view){
+        EditText userNumber = (EditText) findViewById(R.id.userNumber);
+
+        Log.i("userNumber", userNumber.getText().toString());
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
